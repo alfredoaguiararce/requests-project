8e83776a8baa6e43f8418dbf4ddcb7b7825e0871
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-08-10 16:25:49 -0700

    :construction: Using of requests

---
+++ store.py
@@ -0,0 +1,10 @@
+import requests as req
+
+def get_categories():
+    r = req.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json()
+    for category in categories:
+        print(category["name"])
